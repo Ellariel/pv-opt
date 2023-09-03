a92384bae022f76dbe6ee0177fa10520de05146d
Ellariel <ell.science@mail.ru>
2023-09-03 15:16:39 +0200

    h

--- gui.build.bat
+++ gui.build.bat
@@ -1,1 +1,9 @@
 python -m nuitka gui.py --standalone
+copy consumption gui.dist\
+copy production gui.dist\
+copy pv_outputs gui.dist\
+copy solution gui.dist\
+copy static gui.dist\
+copy template gui.dist\
+copy uploaded gui.dist\
+copy pv_inputs.csv gui.dist\
